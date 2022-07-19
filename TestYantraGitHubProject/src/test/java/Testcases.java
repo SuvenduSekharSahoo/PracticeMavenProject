4837712db1fb37ab3e0329b7331b299a55d63034
Dell <Dell@DESKTOP-B2UQ05A>
2022-07-19 10:02:16 +0530

    added sopln

--- TestYantraGitHubProject/src/test/java/Testcases.java
+++ TestYantraGitHubProject/src/test/java/Testcases.java
@@ -10,6 +10,7 @@ public class Testcases {
 System.out.println("i am lohith");
 
 System.out.println("Hi Amit changes made in Contact module by Pavithra");
+	System.out.println("It's Suvendu");
 
 	}
 
